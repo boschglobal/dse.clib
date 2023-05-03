0b2d2b971f4d2359811df17a80bc1a0f7684213c
Timothy Rule <90191439+timrulebosch@users.noreply.github.com>
2023-05-03 15:43:08 +0200

    Create testing.h for unified testing support across the DSE.

---
+++ dse/testing.h
@@ -0,0 +1,18 @@
+// Copyright 2023 Robert Bosch GmbH
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef DSE_TESTING_H_
+#define DSE_TESTING_H_
+
+
+#ifdef UNIT_TESTING
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#endif  // UNIT_TESTING
+
+
+#endif  // DSE_TESTING_H_
