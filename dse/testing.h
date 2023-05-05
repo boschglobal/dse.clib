404729ce3d1ee47e65efa65610416b1f9e0b4ee9
Timothy Rule <90191439+timrulebosch@users.noreply.github.com>
2023-05-05 08:14:42 +0200

    Add a preprocess swap for strdup to support CMocka tests.
    The CMocka memory leak check does not work with strdup, and wrapping strdup with the linker may not work with certain libraries. Therefore use the preprocessor to swap the implementation when unit testing.

--- dse/testing.h
+++ dse/testing.h
@@ -5,16 +5,34 @@
 #ifndef DSE_TESTING_H_
 #define DSE_TESTING_H_
 
-
 #ifdef UNIT_TESTING
+
+
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <float.h>
+#include <string.h>
 #include <setjmp.h>
 #include <cmocka.h>
-#endif  // UNIT_TESTING
 
+
+/* Wrapping strdup() to get memory checking does not work with libyaml (and
+ * perhaps other libs too). Therefore, rather than wrapping we can use the
+ * preprocessor to swap strdup with this implementation in DSE code only.
+ */
+static inline char *dse_strdup_swap(const char *s)
+{
+    if (s == NULL) return NULL;
+    size_t len = strlen(s) + 1;
+    void  *dup = malloc(len);
+    if (dup == NULL) return NULL;
+    return (char *)memcpy(dup, s, len);
+}
+#define strdup dse_strdup_swap
+
+
+#endif  // UNIT_TESTING
 
 #endif  // DSE_TESTING_H_
