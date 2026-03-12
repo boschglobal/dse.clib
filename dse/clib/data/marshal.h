94c560f33c051fc0269e0dd05ac5477a14ce0295
Timothy Rule (VM/EMT3) <Timothy.Rule@de.bosch.com>
2026-03-12 14:37:54 +0100

    Add offset to MarshalSignalMap, logging only.
    Signed-off-by: Timothy Rule (VM/EMT3) <Timothy.Rule@de.bosch.com>

--- dse/clib/data/marshal.h
+++ dse/clib/data/marshal.h
@@ -255,8 +255,21 @@ typedef struct MarshalSignalMap {
         uint32_t* binary_len;
     } source;
 
+    /* Offset of source relative to its container. Logging only. */
+    size_t offset;
+
     /* Reserved. */
-    uint64_t __reserved__[4];
+#if defined(__x86_64__)
+#if __SIZEOF_POINTER__ == 8
+    uint64_t __reserved__[3];
+#else
+    uint32_t __reserved_4__;
+    uint64_t __reserved__[3];
+#endif
+#elif defined(__i386__)
+    uint32_t __reserved_4__;
+    uint64_t __reserved__[3];
+#endif
 } MarshalSignalMap;
 
 
